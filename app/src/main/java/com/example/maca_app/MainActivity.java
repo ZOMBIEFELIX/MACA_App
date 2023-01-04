aafbed7d2d1f30b411a45a7e8fdc6fe901424874
ZOMBIEFELIX <FELIXBARRADAS@Outlook.com>
2023-01-04 04:21:45 -0600

    Casi lo logro

--- app/src/main/java/com/example/maca_app/MainActivity.java
+++ app/src/main/java/com/example/maca_app/MainActivity.java
@@ -7,8 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView nuevoUsuario, bienvenidoLabel, continuarLabel;
+    ImageView LoginImageView;
+    TextInputLayout usuarioTextfield, contrasenaTextField;
+    MaterialButton inicioSesion;
 
     private Button Login;
 
@@ -18,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         Login = findViewById(R.id.btnLogin);
+
+        LoginImageView= findViewById(R.id.LoginImageView);
+        bienvenidoLabel = findViewById(R.id.bienvenidoLabel);
+        continuarLabel = findViewById(R.id.continuarLabel);
+        usuarioTextfield = findViewById(R.id.usuarioTextfield);
+        contrasenaTextField = findViewById(R.id.contrasenaTextfield);
+        inicioSesion = findViewById(R.id.iniciaSesion);
+        nuevoUsuario = findViewById(R.id.nuevoUsuario);
+
+        nuevoUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            }
+        });
+
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/maca_app/SignUpActivity.java
+++ app/src/main/java/com/example/maca_app/SignUpActivity.java
@@ -2,13 +2,50 @@ package com.example.maca_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    TextView nuevoUsuario, bienvenidoLabel, continuarLabel;
+    ImageView signUpImageView;
+    TextInputLayout usuarioSignUpTextField, contrasenaTextField;
+    MaterialButton inicioSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signUpImageView = findViewById(R.id.logo2);
+        bienvenidoLabel = findViewById(R.id.bienvenidoLabel);
+        continuarLabel = findViewById(R.id.continuarLabel);
+        usuarioSignUpTextField = findViewById(R.id.usuarioSignUpTextField);
+        contrasenaTextField = findViewById(R.id.contrasenaTextfield);
+        inicioSesion = findViewById(R.id.inicioSesion);
+        nuevoUsuario = findViewById(R.id.nuevoUsuario);
+
+       nuevoUsuario.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               transitionBack();
+           }
+       });
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        transitionBack();
+    }
+
+    public void transitionBack(){
+        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
     }
 }
